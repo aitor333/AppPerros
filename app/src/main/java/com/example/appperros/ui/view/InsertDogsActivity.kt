ea648763a750871a610af192e5c6dd505b70275d
Aitor Parada Linaje <aitorparadalinaje002@gmail.com>
2023-05-11 19:09:14 +0200

    Actualizacion de validacion y spinner de sexo

--- app/src/main/java/com/example/appperros/ui/view/InsertDogsActivity.kt
+++ app/src/main/java/com/example/appperros/ui/view/InsertDogsActivity.kt
@@ -3,6 +3,8 @@ package com.example.appperros.ui.view
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.appperros.databinding.ActivityInsertDogsBinding
@@ -11,43 +13,71 @@ import com.example.appperros.help.Conexion.connectionclass
 
 class InsertDogsActivity : AppCompatActivity() {
     private var binding: ActivityInsertDogsBinding? = null
+    lateinit var sexoSeleccionado:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityInsertDogsBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
         mostrarErrores()
 
+        binding!!.spSexo.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                adapterView: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+
+                sexoSeleccionado = adapterView?.getItemAtPosition(position).toString()
+
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                Toast.makeText(
+                    applicationContext,
+                    "No se ha seleccionado ninguna opcion",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        }
+
         //Insert
         binding!!.btnInsertar.setOnClickListener {
-            if(validarCampos()){
-                Toast.makeText(applicationContext,"Fallo al rellenar los campos",Toast.LENGTH_SHORT).show()
+            if (validarCampos()) {
+                Toast.makeText(
+                    applicationContext,
+                    "Fallo al rellenar los campos",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
 
 
-                    val connection = connectionclass()
-                    try {
-                        if (connection != null) {
-                            val query =
-                                "insert into perro (nombreperro,nombreraza,edad,nchip,sexo,peso) VALUES (?,?,?,?,?,?)"
-                            val statement = connection.prepareStatement(query)
-                            statement.setString(1, binding!!.edtxtNombre.text.toString())
-                            statement.setString(2, binding!!.edtxtRaza.text.toString())
-                            statement.setInt(3, binding!!.edtxtEdad.text.toString().toInt())
-                            statement.setString(4, binding!!.edtxtNChip.text.toString())
-                            statement.setString(5, binding!!.edtxtSexo.text.toString())
-                            statement.setInt(6, binding!!.edtxtPeso.text.toString().toInt())
-                            val rowsAffected = statement.executeUpdate()
+            val connection = connectionclass()
+            try {
+                if (connection != null) {
+                    val query =
+                        "insert into perro (nombreperro,nombreraza,edad,nchip,sexo,peso) VALUES (?,?,?,?,?,?)"
+                    val statement = connection.prepareStatement(query)
+                    statement.setString(1, binding!!.edtxtNombre.text.toString())
+                    statement.setString(2, binding!!.edtxtRaza.text.toString())
+                    statement.setInt(3, binding!!.edtxtEdad.text.toString().toInt())
+                    statement.setString(4, binding!!.edtxtNChip.text.toString())
+                    statement.setString(5, sexoSeleccionado)
+                    statement.setInt(6, binding!!.edtxtPeso.text.toString().toInt())
+                    val rowsAffected = statement.executeUpdate()
 
-                            Toast.makeText(
-                                applicationContext,
-                                "Registro insertado coreectamente",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                            connection.close()
-                        }
-                    } catch (exception: Exception) {
-                        Log.e("Error en el registro", exception.message!!)
-                    }
+                    Toast.makeText(
+                        applicationContext,
+                        "Registro insertado coreectamente",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    connection.close()
+                }
+            } catch (exception: Exception) {
+                Log.e("Error en el registro", exception.message!!)
+            }
 
 
             binding!!.btnBackInsertar.setOnClickListener {
@@ -58,15 +88,23 @@ class InsertDogsActivity : AppCompatActivity() {
 
     private fun mostrarErrores() {
         var listaErrores = Conexion.errores
-        for (error in listaErrores){
-            if(error.equals("Network error IOException: failed to connect to /192.168.1.132 (port 1433) from /:: (port 53724): connect failed: ETIMEDOUT (Connection timed out)")){
-                Toast.makeText(this@InsertDogsActivity,"Fallo en la conexion al servidor de la bd ",Toast.LENGTH_SHORT).show()
+        for (error in listaErrores) {
+            if (error.equals("Network error IOException: failed to connect to /192.168.1.132 (port 1433) from /:: (port 53724): connect failed: ETIMEDOUT (Connection timed out)")) {
+                Toast.makeText(
+                    this@InsertDogsActivity,
+                    "Fallo en la conexion al servidor de la bd ",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
     }
 
-    private fun validarCampos() : Boolean{
-        return !(binding!!.edtxtEdad.text.toString().isEmpty() && binding!!.edtxtNombre.text.toString().isEmpty() &&binding!!.edtxtRaza.text.toString().isEmpty() && binding!!.edtxtNChip.text.toString().isEmpty() && binding!!.edtxtSexo.text.toString().isEmpty() && binding!!.edtxtPeso.text.toString().isEmpty())
+    private fun validarCampos(): Boolean {
+        return !(binding!!.edtxtEdad.text.toString()
+            .isEmpty() && binding!!.edtxtNombre.text.toString()
+            .isEmpty() && binding!!.edtxtRaza.text.toString()
+            .isEmpty() && binding!!.edtxtNChip.text.toString()
+            .isEmpty() && binding!!.edtxtPeso.text.toString().isEmpty())
     }
 
     override fun onBackPressed() {
